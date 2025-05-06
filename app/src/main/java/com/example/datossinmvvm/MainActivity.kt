98d671d79e4ce345f5ab09f7bd36a8a1c44733cb
CarlosHuamaniEstudianteTec <carlos.huamani.pi@tecsup.edu.pe>
2025-05-05 22:25:56 -0500

    Integrate user management UI with Room database
    This commit replaces the previous greeting message with a `ScreenUser` composable function, and uses `MaterialTheme` instead `DatosSinMVVMTheme`. It also includes a preview of the user management screen.

--- app/src/main/java/com/example/datossinmvvm/MainActivity.kt
+++ app/src/main/java/com/example/datossinmvvm/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -16,32 +17,18 @@ import com.example.datossinmvvm.ui.theme.DatosSinMVVMTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            DatosSinMVVMTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+            MaterialTheme {
+                ScreenUser()
             }
         }
     }
 }
 
+@Preview
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    DatosSinMVVMTheme {
-        Greeting("Android")
+fun ScreenUserPreview() {
+    MaterialTheme {
+        ScreenUser()
     }
 }
